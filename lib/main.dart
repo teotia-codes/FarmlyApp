abed618f1229ef343a21386a9005c2002f864309
teotia-codes <priyanshu.teotia50@gmail.com>
2024-09-08 19:52:33 +0530

    razorpay  integerated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:app/onboarding.dart';
+import 'package:app/razorpay/razorpay.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const FoochiOnboardingView(),
+      home: RazorPayPage(),
     );
   }
 }
